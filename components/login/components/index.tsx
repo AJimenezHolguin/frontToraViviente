690a0dc6e570a0176f44e8b3c752a962d9194b3f
anderson <anjimhol23@gmail.com>
2025-04-03 15:42:20 +0100

    resolviendo conflictos

--- components/login/components/index.tsx
+++ components/login/components/index.tsx
@@ -1,30 +1,19 @@
-<<<<<<< HEAD
-import Image from "next/image";
-=======
 "use client";
 import Image from "next/image";
 import { useState } from "react";
 import { Form } from "@heroui/form";
 import { useRouter } from "next/navigation";
->>>>>>> e0d2a0c17be9e946e1aae4c1b20116c39881435f
 
 import { ClassNameKeys } from "../../../types/classNamesKeys";
 import { InputComponent } from "../../../shared/components/input/input";
 import { ButtonComponent } from "../../../shared/components/button/button";
-<<<<<<< HEAD
-=======
 import { handleLogin } from "../domain/actions/authActions";
->>>>>>> e0d2a0c17be9e946e1aae4c1b20116c39881435f
 
 import {
   LabelPlacementProps,
   TypeProps,
   VariantProps,
 } from "@/shared/components/input/types";
-<<<<<<< HEAD
-import { Sizes } from "@/types/sizes.enum";
-=======
->>>>>>> e0d2a0c17be9e946e1aae4c1b20116c39881435f
 import { Colors } from "@/types/color.enum";
 import { Text } from "@/shared/components/Text";
 import { RadiusProps } from "@/types/radius.enum";
@@ -32,72 +21,6 @@ import { CheckboxComponent } from "@/shared/components/Checkbox";
 import { COLORS } from "@/shared/styles/colors";
 
 export const Login = () => {
-<<<<<<< HEAD
-  return (
-    <section className="m-[25px] h-full">
-      <Image
-        alt="Tora-Viviente"
-        height={60}
-        src={"/logo-tora-viviente-sin-fondo.png"}
-        width={60}
-      />
-      <Text $v="h1" className={"mt-6"}>
-        Sign In
-      </Text>
-      <InputComponent
-        classNames={{
-          [ClassNameKeys.BASE]: "pt-6",
-        }}
-        color={Colors.PRIMARY}
-        label="E-mail"
-        labelPlacement={LabelPlacementProps.OUTSIDE}
-        placeholder={"example@gmail.com"}
-        size={Sizes.SM}
-        type={TypeProps.EMAIL}
-        variant={VariantProps.BORDERED}
-      />
-      <InputComponent
-        classNames={{
-          [ClassNameKeys.BASE]: "pt-6",
-        }}
-        color={Colors.PRIMARY}
-        label="Password"
-        labelPlacement={LabelPlacementProps.OUTSIDE}
-        placeholder={"@#*%"}
-        radius={RadiusProps.SM}
-        size={Sizes.SM}
-        type={TypeProps.PASSWORD}
-        variant={VariantProps.BORDERED}
-      />
-      <div className="flex justify-between pt-6">
-        <CheckboxComponent
-          classNames={{
-            [ClassNameKeys.BASE]: "pt-8",
-          }}
-          color={Colors.PRIMARY}
-          size={Sizes.SM}
-        >
-          Remember me
-        </CheckboxComponent>
-        <Text
-          $color={COLORS.primary}
-          $v="sm"
-          className={
-            "mt-6 font-bold underline decoration-black-500 underline-offset-4"
-          }
-        >
-          Forgot Password?
-        </Text>
-      </div>
-      <ButtonComponent
-        className="mt-[45px] "
-        fullWidth={true}
-        radius={RadiusProps.FULL}
-      >
-        Sign In
-      </ButtonComponent>
-    </section>
-=======
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -218,6 +141,5 @@ export const Login = () => {
         </div>
       </section>
     </div>
->>>>>>> e0d2a0c17be9e946e1aae4c1b20116c39881435f
   );
 };
