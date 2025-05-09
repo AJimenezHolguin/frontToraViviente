3c689a92350e35cbfd89db4e4c6a06d9f037935f
anderson <anjimhol23@gmail.com>
2025-05-09 16:07:26 +0100

    Refactor Modal component types to remove unnecessary newlines and align properties

--- shared/components/Modal/types.ts
+++ shared/components/Modal/types.ts
@@ -1,30 +1,26 @@
 import { Song } from "@/types/SongsTypesProps";
 
-
 export interface ModalSongProps {
-    isOpen: boolean;
-    setIsOpen: (open: boolean) => void;
-    onClose: () => void;
-    onSongCreated: () => void;
-    songToEdit?: Song | null;
+  isOpen: boolean;
+  setIsOpen: (open: boolean) => void;
+  onClose: () => void;
+  onSongCreated: () => void;
+  songToEdit?: Song | null;
 }
-
 
 type AlertType = "success" | "error";
 
 export interface AlertModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    type: AlertType;
-    message: string;
-    
-  
-  }
+  isOpen: boolean;
+  onClose: () => void;
+  type: AlertType;
+  message: string;
+}
 
-  export interface ConfirmModalProps {
-    isOpen: boolean;
-    onClose: () => void;
-    onConfirm: () => void;
-    message: string;
-    isLoading?: boolean;
-  }
+export interface ConfirmModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  message: string;
+  isLoading?: boolean;
+}
