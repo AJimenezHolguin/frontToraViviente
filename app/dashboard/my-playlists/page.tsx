9be51d5db798dcf42b0bf44c42740b787fe7303b
anderson <anjimhol23@gmail.com>
2025-05-21 17:32:17 +0100

    Refactor MyPlayListsPage to use separate component for MyPlayLists

--- app/dashboard/my-playlists/page.tsx
+++ app/dashboard/my-playlists/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { MyPlayLists } from '@/components/myPlayLists/components'
+
 
 export default function MyPlayListsPage() {
   return (
     <div>
       <h1>Hello My Play List</h1>
+      <MyPlayLists />
     </div>
   )
 }
---
+++ components/myPlayLists/components/index.tsx
@@ -0,0 +1,5 @@
+export const MyPlayLists = () => {
+    return (
+        <h1>hola mundo</h1>
+    )
+}
