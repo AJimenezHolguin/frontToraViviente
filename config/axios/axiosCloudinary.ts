661c5c980c81cc4cabbdce3d24c7ce4d527e47fe
anderson <anjimhol23@gmail.com>
2025-06-11 08:54:53 +0100

    Refactor login component layout and update Cloudinary base URL configuration

--- config/axios/axiosCloudinary.ts
+++ config/axios/axiosCloudinary.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const currentEnv = "dks7wxcny"
 
 const axiosCloudinary = axios.create({
-    baseURL: "https://api.cloudinary.com/v1_1/dks7wxcny",
+    baseURL: `https://api.cloudinary.com/v1_1/${currentEnv}`,
     headers: {
         "Content-Type": "multipart/form-data",
     }
