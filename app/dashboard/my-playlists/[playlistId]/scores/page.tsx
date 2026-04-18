c17649d0b2baf60415d05b9ba3c13718ac5f8eba
anderson <anjimhol23@gmail.com>
2026-04-17 21:55:22 -0500

    feat: update songs and scores pages to use playlistId prop instead of id

--- app/dashboard/my-playlists/[playlistId]/scores/page.tsx
+++ app/dashboard/my-playlists/[playlistId]/scores/page.tsx
@@ -1,6 +1,6 @@
 import PlaylistPDFViewer from "@/shared/components/PlaylistPDFViewer";
 
 
-export default function ScoresPage({params}:{params:{id:string}}) {
-    return <PlaylistPDFViewer type="fileScore" playlistId={params.id} />; 
+export default function ScoresPage({params}:{params:{playlistId:string}}) {
+    return <PlaylistPDFViewer type="fileScore" playlistId={params.playlistId} />; 
   }
