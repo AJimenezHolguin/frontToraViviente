fd7a43667ea74d0d87d6a7df5c5bebfe5097182f
anderson <anjimhol23@gmail.com>
2026-04-23 00:31:24 -0500

    Refactor UnauthorizedPage component and implement new Unauthorized component with modal alert

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -1,34 +1,5 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useModalAlert } from "@/shared/hooks/songs/useModalAlert";
-import { ConfirmModal } from "@/shared/components/Modal/ConfirmModal";
+import { Unauthorized } from "@/components/unauthorized/components";
 
 export default function UnauthorizedPage() {
-  const router = useRouter();
-
-  const { showConfirm, ConfirmModalProps } = useModalAlert();
-
-  useEffect(() => {
-    showConfirm(
-      "¡No tienes permisos para acceder a esta página!",
-      () => {
-        router.push("/dashboard/all-playlists"); 
-      },
-      {
-        showCancelButton: false, 
-         titleHeader: "Acceso denegado",
-      }
-    );
-  }, []);
-
-  return (
-    <>
-      <ConfirmModal
-        {...ConfirmModalProps}
-        titleButton="Volver a playlists"
-      />
-    </>
-  );
+  return <Unauthorized />
 }
